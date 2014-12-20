43de1caa2d27b6269891d9901c4342f0c08b1890
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2014-12-20 15:50:30 +0200

    field marking and tests

--- Tic-Tac-Toe/game.py
+++ Tic-Tac-Toe/game.py
@@ -5,3 +5,15 @@ class Game(object):
         self.cp = computers_mark
         self.board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
         self.turns = 9
+
+    def field_taken(self, row, col):
+        return self.board[row][col] != ' '
+
+    def field_exists(self, row, col):
+        return 0 <= row and row <= 2 and 0 <= col and col <= 2
+
+    def player_puts_mark(self, row, col):
+        self.board[row][col] = self.py
+
+    def computer_puts_mark(self, row, col):
+        self.board[row][col] = self.cp
--- Tic-Tac-Toe/game_tests.py
+++ Tic-Tac-Toe/game_tests.py
@@ -1,6 +1,7 @@
 import unittest
 from game import Game
 
+
 class GameTests(unittest.TestCase):
     def setUp(self):
         self.ticky = Game('X', 'O')
@@ -10,6 +11,24 @@ class GameTests(unittest.TestCase):
         self.assertEqual('O', self.ticky.cp)
         self.assertEqual([[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']], self.ticky.board)
         self.assertEqual(self.ticky.turns, 9)
+
+    def test_field_taken(self):
+        self.ticky.board[1][1] = '&'
+        self.assertTrue(self.ticky.field_taken(1, 1))
+        self.assertFalse(self.ticky.field_taken(2, 2))
+
+    def test_field_exists(self):
+        self.assertFalse(self.ticky.field_exists(3, 3))
+        self.assertTrue(self.ticky.field_exists(1, 1))
+
+    def test_player_puts_mark(self):
+        self.ticky.player_puts_mark(1, 1)
+        self.assertEqual(self.ticky.board[1][1], 'X')
+
+    def test_computer_puts_mark(self):
+        self.ticky.computer_puts_mark(1, 1)
+        self.assertEqual(self.ticky.board[1][1], 'O')
+
 
 if __name__ == '__main__':
     unittest.main()
